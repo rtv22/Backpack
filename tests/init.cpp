edc1647aab15576148a646f989d65acbfd912fe2
rtv22 <rtvcojigor@gmail.com>
2017-12-19 12:40:01 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -17,11 +17,11 @@ SCENARIO("copy", "[copy]")
 
 SCENARIO("main", "[main]")
 {
-	Backpack t(300, 3);				//___________MaxW = 300____________
-	Backpack copy(t);				//|   №   |   weight   |    cost   |
+	Backpack test(300, 3);				//___________MaxW = 300____________
+	Backpack copy(test);				//|   №   |   weight   |    cost   |
 	int val[] = { 10, 20, 30 };			//|   1   |    100     |     10    |
 	int wt[] = { 100, 120, 130 };			//|   2   |    120     |     20    |		maxVal = 50
-							//|   3   |    130     |     30    |				
+	REQUIRE(t.knapSack(wt, val, copy) == 50);	//|   3   |    130     |     30    |				
 							//---------------------------------|	
 }
 
