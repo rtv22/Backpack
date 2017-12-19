a40db94505f3731042859396ba9301d4bc13109a
rtv22 <rtvcojigor@gmail.com>
2017-12-19 14:04:20 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -9,10 +9,10 @@ SCENARIO("init", "[init]") {
 
 SCENARIO("copy", "[copy]")
 {
-	Backpack test (100, 2);
+	Backpack test (100, 3);
 	Backpack copy(test);
 	REQUIRE(test.Weight() == 100);
-	REQUIRE(test.Value() == 2);
+	REQUIRE(test.Value() == 3);
 }
 
 SCENARIO("main", "[main]")
