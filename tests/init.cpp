0c4fd42189263946905edc37e5bfb301351c51cf
rtv22 <rtvcojigor@gmail.com>
2017-12-19 11:33:05 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -14,3 +14,14 @@ SCENARIO("copy", "[copy]")
 	REQUIRE(test.Weight() == 100);
 	REQUIRE(test.Value() == 2);
 }
+
+SCENARIO("main", "[main]")
+{
+	Backpack t(300, 3);				//___________MaxW = 300____________
+	Backpack copy(t)				//|   №   |   weight   |    cost   |
+	int val[] = { 10, 20, 30 };			//|   1   |    100     |     10    |
+	int wt[] = { 100, 120, 130 };			//|   2   |    120     |     20    |		maxVal = 50
+							//|   3   |    130     |     30    |				
+							//---------------------------------|	
+}
+
