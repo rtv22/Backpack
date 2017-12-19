846e5c9d282d24da3b87aca95f3c6c04f8bce197
rtv22 <rtvcojigor@gmail.com>
2017-12-19 11:35:58 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -18,7 +18,7 @@ SCENARIO("copy", "[copy]")
 SCENARIO("main", "[main]")
 {
 	Backpack t(300, 3);				//___________MaxW = 300____________
-	Backpack copy(t)				//|   №   |   weight   |    cost   |
+	Backpack copy(t);				//|   №   |   weight   |    cost   |
 	int val[] = { 10, 20, 30 };			//|   1   |    100     |     10    |
 	int wt[] = { 100, 120, 130 };			//|   2   |    120     |     20    |		maxVal = 50
 							//|   3   |    130     |     30    |				
